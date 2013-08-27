e8997f39295a0f9920adae157b432b49e1dc5805
Aldrin Leal <aldrin@leal.eng.br>
2013-08-27 03:59:33 -0300

    Fixing test runner

--- cloudy-data/src/test/java/br/com/ingenieux/cloudy/data/util/SDBTestRunner.java
+++ cloudy-data/src/test/java/br/com/ingenieux/cloudy/data/util/SDBTestRunner.java
@@ -1,19 +1,23 @@
 package br.com.ingenieux.cloudy.data.util;
 
+import java.io.IOException;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Properties;
 
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
-import org.nnsoft.guice.rocoto.configuration.ConfigurationModule;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.inject.name.Names;
 
 public class SDBTestRunner extends BlockJUnit4ClassRunner {
     @Retention(RetentionPolicy.RUNTIME)
@@ -38,10 +42,19 @@ public class SDBTestRunner extends BlockJUnit4ClassRunner {
     }
     
     public Collection<Module> getModules() {
-        ConfigurationModule configurationModule = new ConfigurationModule() {
+        Module configurationModule = new AbstractModule() {
             @Override
-            protected void bindConfigurations() {
-                bindProperties("simplejpa.properties");
+            protected void configure() {
+            	Properties p = new Properties();
+            	
+            	try {
+					p.load(getClass().getResourceAsStream("/simplejpa.properties"));
+				} catch (IOException e1) {
+					throw new RuntimeException(e1);
+				}
+            	
+            	for (Map.Entry<Object, Object> e : p.entrySet())
+            		bind(String.class).annotatedWith(Names.named("" + e.getKey())).toInstance("" + e.getValue());
             }
         };
 
